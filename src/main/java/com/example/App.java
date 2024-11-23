08413fb6850762d645a02a1a8b593b01da008c52
Zimina Olesia <ol3siazim@yandex.ru>
2024-11-24 01:35:35 +0300

    first commit

--- src/main/java/com/example/App.java
+++ src/main/java/com/example/App.java
@@ -1,6 +1,13 @@
 package com.example;
-    public class App {
-        public static void main(String[] args) {
+
+public class App {
+    public static void main(String[] args) {
         System.out.println("Hello, Maven!");
+        // Чтобы приложение не завершалось сразу, добавьте задержку
+        try {
+            Thread.sleep(60000); // Задержка на 60 секунд для тестирования
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
     }
 }
