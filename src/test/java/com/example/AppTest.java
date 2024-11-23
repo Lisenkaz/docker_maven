80e7c94b2f9604c652807d5dc90bf00dbffd94db
Zimina Olesia <ol3siazim@yandex.ru>
2024-11-24 00:47:06 +0300

    first commit

--- src/test/java/com/example/AppTest.java
+++ src/test/java/com/example/AppTest.java
@@ -1,7 +1,7 @@
 package com.example;
 
-import org.junit.Test;
-import static org.junit.Assert.assertTrue;
+import org.junit.jupiter.api.Test; // Импорт для JUnit 5
+import static org.junit.jupiter.api.Assertions.assertTrue; // Импорт для утверждений
 
 public class AppTest {
     
